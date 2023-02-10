3b0aa1e1940bae92a62283e86311a363cd01d431
Lesther1 <lesthercrespo@gmail.com>
2023-02-10 18:08:05 -0500

    actualizacion de back

--- app/src/components/List.jsx
+++ app/src/components/List.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Todo from "./Todo";
+import axios from "axios";
 
 const List = ({ list,  removeTodoListProp, editTodoListProp }) => {
   const renderedList = list.map((item) => (
     <Todo
       title={item.title}
       completed={item.completed}
-      removeTodoListProp={(e) => removeTodoListProp(item.title)}
-      editTodoItemProp={(updatedItem) =>
-        editTodoListProp(item._id, updatedItem)
-      }
+      removeTodoListProp={(e) => {
+        // Eliminar el elemento de la base de datos
+        axios.delete(`http://localhost:3030/todos/${item._id}`).then(() => {
+          // Actualizar el estado en el front-end
+          removeTodoListProp(item.title);
+        });
+      }}
       key={item.title}
     />
   ));
